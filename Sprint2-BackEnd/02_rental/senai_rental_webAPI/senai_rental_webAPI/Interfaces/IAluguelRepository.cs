b26d3c6e1775ac7712dc5f4e7656d5f14cb43cea
Wesley <wmixgamer@gmail.com>
2021-09-01 23:04:00 -0300

    Interface Aluguel Criada

---
+++ Sprint2-BackEnd/02_rental/senai_rental_webAPI/senai_rental_webAPI/Interfaces/IAluguelRepository.cs
@@ -0,0 +1,17 @@
+﻿using senai_rental_webAPI.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_rental_webAPI.Interfaces
+{
+    interface IAluguelRepository
+    {
+        List<AluguelDomain> ListarTodos();
+        AluguelDomain BuscarPorId(int idAluguel);
+        void Cadastrar(AluguelDomain novoAluguel);
+        void AtualizarIdUrl(int idGenero, AluguelDomain aluguelAtualizado);
+        void Deletar(int idAluguel);
+    }
+}
